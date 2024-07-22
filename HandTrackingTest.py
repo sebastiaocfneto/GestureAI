48ab51712169c7ae1e71a615787a0c415b6b3239
felipelagares <62161177+felipelagares@users.noreply.github.com>
2024-07-22 18:32:51 -0300

    Update HandTrackingTest.py

--- HandTrackingTest.py
+++ HandTrackingTest.py
@@ -18,42 +18,42 @@ scale = 0
 # coordenadas do inicio da imagem
 cx, cy = 0, 0
 
-dedos = {'polegar': 0,
-         'indicador': 1,
-         'médio': 2,
-         'anelar': 3,
-         'minimo': 4
-         }
+ponta_dedos = {'polegar': 4,
+               'indicador': 8,
+               'medio': 12,
+               'anelar': 16,
+               'minimo': 20
+               }
 # formato: dedos_levantados, dedos_a_analisar, distancia_ponta_centro_x, distancia_ponta_centro_y, movimento_x, movimento_y
 #positivo: direita, cima
 letras = {
-    'A': [[0, 0, 0, 0, 0], 'polegar', 1, 5, 0, 0],
-    'B': [[0, 1, 1, 1, 1], 'polegar', 1, 1, 0, 0],
-    'C': [[1, 1, 1, 1, 1], 'medio', 1, 1, 0, 0],
-    'Ç': [[1, 1, 1, 1, 1], 'medio', 1, 1, 1, 1],
-    'D': [[1, 1, 0, 0, 0], 'medio', 1, 1, 0, 0],
-    'E': [[0, 0, 0, 0, 0], 'medio', 0, 1, 0, 0],
-    'F': [[1, 1, 1, 1, 1], 'indicador', 1, 1, 0, 0],
-    'G': [[0, 1, 0, 0, 0], 'indicador', 0, 5, 0, 0],
-    'H': [[1, 1, 1, 0, 0], 'medio', 5, 1, 5, 0],
-    'I': [[0, 0, 0, 0, 1], 'minimo', 1, 5, 0, 0, ],
-    'J': [[0, 0, 0, 0, 1], 'minimo', 0, 0, 0, -5],
-    'K': [[1, 1, 1, 0, 0], 'medio', 5, 1, 0, 5],
-    'L': [[1, 1, 0, 0, 0], 'polegar', 5, 1, 0, 0],
-    'M': [[0, 1, 1, 1, 0], 'medio', 0, -5, 0, 0],
-    'N': [[0, 1, 1, 0, 0], 'medio', 0, -5, 0, 0],
-    'O': [[1, 1, 1, 1, 1], 'polegar', 5, 1, 0, 0],
-    'P': [[1, 1, 1, 0, 0], 'medio', -5, -1, 0, 0],
-    'Q': [[0, 1, 0, 0, 0], 'indicador', 0, -5, 0, 0],
-    'R': [[0, 1, 1, 0, 0], 'indicador', -1, 5, 0, 0],
+    'A': [[0, 0, 0, 0, 0], 'polegar', 10, 30, 0, 0],
+    'B': [[0, 1, 1, 1, 1], 'polegar', 10, 10, 0, 0],
+    'C': [[1, 1, 1, 1, 1], 'medio', 10, 10, 0, 0],
+    'Ç': [[1, 1, 1, 1, 1], 'medio', 10, 10, 10, 10],
+    'D': [[1, 1, 0, 0, 0], 'medio', 10, 10, 0, 0],
+    'E': [[0, 0, 0, 0, 0], 'medio', 0, 10, 0, 0],
+    'F': [[1, 1, 1, 1, 1], 'indicador', 10, 10, 0, 0],
+    'G': [[0, 1, 0, 0, 0], 'indicador', 0, 50, 0, 0],
+    'H': [[1, 1, 1, 0, 0], 'medio', 50, 10, 50, 0],
+    'I': [[0, 0, 0, 0, 1], 'minimo', 10, 50, 0, 0, ],
+    'J': [[0, 0, 0, 0, 1], 'minimo', 0, 0, 0, -50],
+    'K': [[1, 1, 1, 0, 0], 'medio', 50, 10, 0, 5],
+    'L': [[1, 1, 0, 0, 0], 'polegar', 50, 10, 0, 0],
+    'M': [[0, 1, 1, 1, 0], 'medio', 0, -50, 0, 0],
+    'N': [[0, 1, 1, 0, 0], 'medio', 0, -20, 0, 0],
+    'O': [[1, 1, 1, 1, 1], 'polegar', 60, 20, 0, 0],
+    'P': [[1, 1, 1, 0, 0], 'medio', -50, -10, 0, 0],
+    'Q': [[0, 1, 0, 0, 0], 'indicador', 0, -50, 0, 0],
+    'R': [[0, 1, 1, 0, 0], 'indicador', -10, 50, 0, 0],
     'S': [[0, 0, 0, 0, 0], 'polegar', 0, 0, 0, 0],
-    'T': [[1, 1, 1, 1, 1], 'indicador', 1, 1, 0, 0],
+    'T': [[1, 1, 1, 1, 1], 'indicador', 10, 10, 0, 0],
     'U': [[0, 1, 1, 0, 0], 'medio', 0, 0, 0, 0],
-    'V': [[0, 1, 1, 0, 0], 'medio', 1, 0, 0, 0],
-    'X': [[0, 1, 0, 0, 0], 'indicador', -5, 0, -5, 0],
-    'W': [[0, 1, 1, 1, 0], 'polegar', 0, 0, 0, 5],
-    'Y': [[1, 0, 0, 0, 1], 'minimo', 0, 5, 5, 0],
-    'Z': [[0, 1, 0, 0, 0], 'indicador', 0, 5, -1, -1]
+    'V': [[0, 1, 1, 0, 0], 'medio', 20, 0, 0, 0],
+    'X': [[0, 1, 0, 0, 0], 'indicador', -50, 0, -50, 0],
+    'W': [[0, 1, 1, 1, 0], 'polegar', 0, 0, 0, 50],
+    'Y': [[1, 0, 0, 0, 1], 'minimo', 0, 50, 40, 0],
+    'Z': [[0, 1, 0, 0, 0], 'indicador', 0, 50, -10, -10]
 
 }
 
@@ -72,13 +72,29 @@ while True:
     # detecção das mãoes
     hands, img = detector.findHands(img)
     # confere se há mãos na tela
-    if len(hands) >= 1:
+    if hands:
+        previous_position = {
+            'polegar': hands[0]['lmList'][ponta_dedos['polegar']],
+            'indicador': hands[0]['lmList'][ponta_dedos['indicador']],
+            'medio': hands[0]['lmList'][ponta_dedos['medio']],
+            'anelar': hands[0]['lmList'][ponta_dedos['anelar']],
+            'minimo': hands[0]['lmList'][ponta_dedos['minimo']],
+
+        }
 
         for letra, valor in letras.items():
-            distX = calcDistX(hands[0][dedos[valor[1]]], hands[0]['center'])
-            distY = calcDistY(hands[0][dedos[valor[1]]], hands[0]['center'])
-            if detector.fingersUp(hands[0]) == valor[0]:
-                print(letra)
+            hand = hands[0]
+
+            if len(hand['lmList']) > 0:  # Certifica-se de que há landmarks detectados
+                point = hand['lmList'][ponta_dedos[valor[1]]]
+                center = hand['center']
+                distX = calcDistX(point, center)
+                distY = calcDistY(point, center)
+                mov_x = calcDistX(point, previous_position[valor[1]])
+                mov_y = calcDistY(point, previous_position[valor[1]])
+                fingers = detector.fingersUp(hand)
+                if fingers == valor[0] and distX >= valor[2] and distY >= valor[3] and mov_x >= valor[4] and mov_y >= valor[5]:
+                    print(f"Letra detectada: {letra}")
 
     # espelhando a imagem horizontalmente para melhor localização
     img = cv2.flip(img, 1)
