3d1b57bb58ed5199a4ba796ced400374c709fadd
felipelagares <62161177+felipelagares@users.noreply.github.com>
2024-07-22 13:24:05 -0300

    ideia de mapping

--- HandTrackingTest.py
+++ HandTrackingTest.py
@@ -16,12 +16,43 @@ scale = 0
 # coordenadas do inicio da imagem
 cx, cy = 0, 0
 
+dedos = {'polegar': 0,
+         'indicador': 1,
+         'médio': 2,
+         'anelar': 3,
+         'minimo': 4
+         }
+# formato: dedos_levantados, dedos_a_analisar, distancia_ponta_centro_x, distancia_ponta_centro_y, movimento_x, movimento_y
+#positivo: direita, cima
 letras = {
-    'B': [0, 1, 1, 1, 1],
-    'G': [0, 1, 0, 0, 0],
-    'I': [0, 0, 0, 0, 1],
-    'L': [1, 1, 0, 0, 0],
-    'U': [0, 1, 1, 0, 0]
+    'A': [[0, 0, 0, 0, 0], 'polegar', 1, 5, 0, 0],
+    'B': [[0, 1, 1, 1, 1], 'polegar', 1, 1, 0, 0],
+    'C': [[1, 1, 1, 1, 1], 'medio', 1, 1, 0, 0],
+    'Ç': [[1, 1, 1, 1, 1], 'medio', 1, 1, 1, 1],
+    'D': [[1, 1, 0, 0, 0], 'medio', 1, 1, 0, 0],
+    'E': [[0, 0, 0, 0, 0], 'medio', 0, 1, 0, 0],
+    'F': [[1, 1, 1, 1, 1], 'indicador', 1, 1, 0, 0],
+    'G': [[0, 1, 0, 0, 0], 'indicador', 0, 5, 0, 0],
+    'H': [[1, 1, 1, 0, 0], 'medio', 5, 1, 5, 0],
+    'I': [[0, 0, 0, 0, 1], 'minimo', 1, 5, 0, 0, ],
+    'J': [[0, 0, 0, 0, 1], 'minimo', 0, 0, 0, -5],
+    'K': [[1, 1, 1, 0, 0], 'medio', 5, 1, 0, 5],
+    'L': [[1, 1, 0, 0, 0], 'polegar', 5, 1, 0, 0],
+    'M': [[0, 1, 1, 1, 0], 'medio', 0, -5, 0, 0],
+    'N': [[0, 1, 1, 0, 0], 'medio', 0, -5, 0, 0],
+    'O': [[1, 1, 1, 1, 1], 'polegar', 5, 1, 0, 0],
+    'P': [[1, 1, 1, 0, 0], 'medio', -5, -1, 0, 0],
+    'Q': [[0, 1, 0, 0, 0], 'indicador', 0, -5, 0, 0],
+    'R': [[0, 1, 1, 0, 0], 'indicador', -1, 5, 0, 0],
+    'S': [[0, 0, 0, 0, 0], 'polegar', 0, 0, 0, 0],
+    'T': [[1, 1, 1, 1, 1], 'indicador', 1, 1, 0, 0],
+    'U': [[0, 1, 1, 0, 0], 'medio', 0, 0, 0, 0],
+    'V': [[0, 1, 1, 0, 0], 'medio', 1, 0, 0, 0],
+    'X': [[0, 1, 0, 0, 0], 'indicador', -5, 0, -5, 0],
+    'W': [[0, 1, 1, 1, 0], 'polegar', 0, 0, 0, 5],
+    'Y': [[1, 0, 0, 0, 1], 'minimo', 0, 5, 5, 0],
+    'Z': [[0, 1, 0, 0, 0], 'indicador', 0, 5, -1, -1]
+
 }
 
 while True:
